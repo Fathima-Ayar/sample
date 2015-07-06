04544c4e3be21820116377ded14b2b80c44bab27
Fathima Ayar <fathimaa@qburst.com>
2015-07-06 15:35:23 +0530

    added field

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
 
+  attr_accessible :role
   acts_as_authentic do |c|
      c.crypto_provider = Authlogic::CryptoProviders::BCrypt
   end
   has_many :articles
   has_many :comments
   acts_as_voter
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -52,15 +52,14 @@ ActiveRecord::Schema.define(version: 20150703064832) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",              limit: 255
-    t.string   "email",                 limit: 255
-    t.string   "crypted_password",      limit: 255
-    t.string   "password_salt",         limit: 255
-    t.string   "persistence_token",     limit: 255
-    t.string   "role",                  limit: 255
-    t.string   "password_confirmation", limit: 255
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
+    t.string   "username",          limit: 255
+    t.string   "email",             limit: 255
+    t.string   "crypted_password",  limit: 255
+    t.string   "password_salt",     limit: 255
+    t.string   "persistence_token", limit: 255
+    t.string   "role",              limit: 255
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
   end
 
   create_table "votes", force: :cascade do |t|
